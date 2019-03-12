658cf62e4a2ce74d35fe0840dfa18e1f9bd89d85
Marcin <marcin.osiadacz@outlook.com>
2019-03-12 13:05:30 +0100

    Initial commit

---
+++ pages/category.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.by import By
+
+from .base_page import BasePage
+from .base_element import BaseElement
+
+
+class CategoryPage(BasePage):
+
+    url = "http://testing.todvachev.com/test-cases/items-in-category-drop-down-menu/"
+
+    @property
+    def category_dropdown(self):
+        locator = (By.CSS_SELECTOR, 'select#cat')
+        return BaseElement(self.driver, locator=locator)
